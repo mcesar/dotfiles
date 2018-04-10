0ae7bd8894a5e7e99dd7c685683adb03ac4d33eb
Marcos César de Oliveira <mcesarhm@gmail.com>
2018-04-10 18:52:34 -0300

    Change st font

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DroidSansMonoForPowerline Nerd Font:size=12:antialias=true:autohint=true";
+static char *font = "DroidSansMono Nerd Font:size=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
